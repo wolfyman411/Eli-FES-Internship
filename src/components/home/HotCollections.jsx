25bddef3360730c240646ea0e333b465101f5480
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2026-06-30 11:25:32 -0400

    API call and display functionality

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 
+import axios from "axios"
+
 const HotCollections = () => {
+
+  const [collections,setCollections] = useState([])
+
+  useEffect(() => {
+    getItems()
+  },[])
+
+  async function getItems() {
+    const {data} = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections")
+    setCollections(data)
+  }
+
+  function collectionHTML(item) {
+    return (
+      <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={item.id}>
+        <div className="nft_coll">
+          <div className="nft_wrap">
+            <Link to="/item-details">
+              <img src={item.nftImage} className="lazy img-fluid" alt="" />
+            </Link>
+          </div>
+          <div className="nft_coll_pp">
+            <Link to="/author">
+              <img className="lazy pp-coll" src={item.authorImage} alt="" />
+            </Link>
+            <i className="fa fa-check"></i>
+          </div>
+          <div className="nft_coll_info">
+            <Link to="/explore">
+              <h4>{item.title}</h4>
+            </Link>
+            <span>ERC-{item.code}</span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,28 +54,8 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
-              </div>
-            </div>
+          {collections.map((item) => (
+            collectionHTML(item)
           ))}
         </div>
       </div>
